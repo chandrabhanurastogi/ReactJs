1ec4b70891a12446256bc55d532bed20d742f810
chandra <rastogi.chandrabhanu@gmail.com>
2017-09-24 12:24:58 +0100

    This is sample app for spring boot and reactjs.

---
+++ demo/src/main/java/tutorial/Employee.java
@@ -0,0 +1,59 @@
+package tutorial;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+ 
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private int age;
+    private int years;
+ 
+    private Employee() {}
+ 
+    public Employee(String name, int age, int years) {
+        this.name = name;
+        this.age = age;
+        this.years = years;
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getYears() {
+		return years;
+	}
+
+	public void setYears(int years) {
+		this.years = years;
+	}
+    
+    
+}
